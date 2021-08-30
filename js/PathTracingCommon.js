42347e0a6064eb061bbd25d065766ddd43a0c7aa
Erich Loftis <erichlof@swbell.net>
2021-08-30 00:37:48 -0500

    Update PathTracingCommon.js

--- js/PathTracingCommon.js
+++ js/PathTracingCommon.js
@@ -24,7 +24,7 @@ precision highp int;
 precision highp sampler2D;
 
 uniform float uOneOverSampleCounter;
-
+uniform float uToneMappingExposure;
 uniform sampler2D accumulationBuffer;
 
 out vec4 glFragColor;
@@ -32,9 +32,7 @@ out vec4 glFragColor;
 // source: https://www.cs.utah.edu/~reinhard/cdrom/
 vec3 ReinhardToneMapping(vec3 color) 
 {
-	// TODO: make 'toneMappingExposure' a uniform
-	float toneMappingExposure = 1.1; 
-	color *= toneMappingExposure;
+	color *= uToneMappingExposure;
 	return clamp(color / (vec3(1) + color), 0.0, 1.0);
 }
 
@@ -367,7 +365,115 @@ uniform float uSampleCounter;
 uniform float uEPS_intersect;
 uniform float uApertureSize;
 uniform float uFocusDistance;
+uniform float uSunAngularDiameterCos;
 uniform bool uCameraIsMoving;
+
+`;
+
+
+BABYLON.Effect.IncludesShadersStore['pathtracing_physical_sky_defines'] = `
+
+#define TURBIDITY 0.5
+#define RAYLEIGH_COEFFICIENT 2.0
+#define MIE_COEFFICIENT 0.03
+#define MIE_DIRECTIONAL_G 0.76
+// constants for atmospheric scattering
+#define THREE_OVER_SIXTEENPI 0.05968310365946075
+#define ONE_OVER_FOURPI 0.07957747154594767
+// wavelength of used primaries, according to preetham
+#define LAMBDA vec3( 680E-9, 550E-9, 450E-9 )
+#define TOTAL_RAYLEIGH vec3( 5.804542996261093E-6, 1.3562911419845635E-5, 3.0265902468824876E-5 )
+// mie stuff
+// K coefficient for the primaries
+#define K vec3(0.686, 0.678, 0.666)
+#define MIE_V 4.0
+#define MIE_CONST vec3( 1.8399918514433978E14, 2.7798023919660528E14, 4.0790479543861094E14 )
+// optical length at zenith for molecules
+#define RAYLEIGH_ZENITH_LENGTH 8400.0
+#define MIE_ZENITH_LENGTH 1250.0
+#define UP_VECTOR vec3(0.0, 1.0, 0.0)
+#define SUN_POWER 200.0
+// 66 arc seconds -> degrees, and the cosine of that
+//#define SUN_ANGULAR_DIAMETER_COS 0.9998 //0.9999566769
+#define CUTOFF_ANGLE 1.6110731556870734
+#define STEEPNESS 1.5
+
+`;
+
+
+
+BABYLON.Effect.IncludesShadersStore['pathtracing_physical_sky_functions'] = `
+
+float RayleighPhase(float cosTheta)
+{
+	return THREE_OVER_SIXTEENPI * (1.0 + (cosTheta * cosTheta));
+}
+
+float hgPhase(float cosTheta, float g)
+{
+        float g2 = g * g;
+        float inverse = 1.0 / pow(max(0.0, 1.0 - 2.0 * g * cosTheta + g2), 1.5);
+	return ONE_OVER_FOURPI * ((1.0 - g2) * inverse);
+}
+
+vec3 totalMie()
+{
+	float c = (0.2 * TURBIDITY) * 10E-18;
+	return 0.434 * c * MIE_CONST;
+}
+
+float SunIntensity(float zenithAngleCos)
+{
+	zenithAngleCos = clamp( zenithAngleCos, -1.0, 1.0 );
+	return SUN_POWER * max( 0.0, 1.0 - pow( E, -( ( CUTOFF_ANGLE - acos( zenithAngleCos ) ) / STEEPNESS ) ) );
+}
+
+vec3 Get_Sky_Color(Ray r, vec3 sunDirection)
+{
+	vec3 viewDirection = normalize(r.direction);
+	
+	/* most of the following code is borrowed from the three.js shader file: SkyShader.js */
+    	// Cosine angles
+	float cosViewSunAngle = dot(viewDirection, normalize(sunDirection));
+    	float cosSunUpAngle = dot(UP_VECTOR, normalize(sunDirection)); // allowed to be negative: + is daytime, - is nighttime
+    	float cosUpViewAngle = dot(UP_VECTOR, viewDirection);
+	
+        // Get sun intensity based on how high in the sky it is
+    	float sunE = SunIntensity(cosSunUpAngle);
+        
+	// extinction (absorbtion + out scattering)
+	// rayleigh coefficients
+    	vec3 rayleighAtX = TOTAL_RAYLEIGH * RAYLEIGH_COEFFICIENT;
+    
+	// mie coefficients
+	vec3 mieAtX = totalMie() * MIE_COEFFICIENT;  
+    
+	// optical length
+	float zenithAngle = acos( max( 0.0, dot( UP_VECTOR, viewDirection ) ) );
+	float inverse = 1.0 / ( cos( zenithAngle ) + 0.15 * pow( 93.885 - ( ( zenithAngle * 180.0 ) / PI ), -1.253 ) );
+	float rayleighOpticalLength = RAYLEIGH_ZENITH_LENGTH * inverse;
+	float mieOpticalLength = MIE_ZENITH_LENGTH * inverse;
+	// combined extinction factor	
+	vec3 Fex = exp(-(rayleighAtX * rayleighOpticalLength + mieAtX * mieOpticalLength));
+	// in scattering
+	vec3 betaRTheta = rayleighAtX * RayleighPhase(cosViewSunAngle * 0.5 + 0.5);
+	vec3 betaMTheta = mieAtX * hgPhase(cosViewSunAngle, MIE_DIRECTIONAL_G);
+	
+	vec3 Lin = pow( sunE * ( ( betaRTheta + betaMTheta ) / ( rayleighAtX + mieAtX ) ) * ( 1.0 - Fex ), vec3( 1.5 ) );
+	Lin *= mix( vec3( 1.0 ), pow( sunE * ( ( betaRTheta + betaMTheta ) / ( rayleighAtX + mieAtX ) ) * Fex, vec3( 1.0 / 2.0 ) ), clamp( pow( 1.0 - cosSunUpAngle, 5.0 ), 0.0, 1.0 ) );
+	// nightsky
+	float theta = acos( viewDirection.y ); // elevation --> y-axis, [-pi/2, pi/2]
+	float phi = atan( viewDirection.z, viewDirection.x ); // azimuth --> x-axis [-pi/2, pi/2]
+	vec2 uv = vec2( phi, theta ) / vec2( 2.0 * PI, PI ) + vec2( 0.5, 0.0 );
+	vec3 L0 = vec3( 0.1 ) * Fex;
+	// composition + solar disc
+	float sundisk = smoothstep( uSunAngularDiameterCos, uSunAngularDiameterCos + 0.00002, cosViewSunAngle );
+	L0 += ( sunE * 19000.0 * Fex ) * sundisk;
+	vec3 texColor = ( Lin + L0 ) * 0.04 + vec3( 0.0, 0.0003, 0.00075 );
+	float sunfade = 1.0 - clamp( 1.0 - exp( ( sunDirection.y / 450000.0 ) ), 0.0, 1.0 );
+	vec3 retColor = pow( texColor, vec3( 1.0 / ( 1.2 + ( 1.2 * sunfade ) ) ) );
+	return retColor;
+}
 
 `;
 
