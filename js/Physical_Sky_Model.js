713e674ab9b1166444f84922d05f1fe3c6e8e3b8
Erich Loftis <erichlof@swbell.net>
2022-05-06 23:33:00 -0500

    Update Physical_Sky_Model.js

--- js/Physical_Sky_Model.js
+++ js/Physical_Sky_Model.js
@@ -13,6 +13,7 @@ let camera, oldCameraMatrix, newCameraMatrix;
 let camFlightSpeed; // scene specific, depending on scene size dimensions
 let cameraRecentlyMoving = false;
 let windowIsBeingResized = false;
+let beginningFlag = true;
 let timeInSeconds = 0.0;
 let frameTime = 0.0;
 let newWidth, newHeight;
@@ -221,10 +222,6 @@ canvas = document.getElementById("renderCanvas");
 
 engine = new BABYLON.Engine(canvas, true);
 
-engine.setHardwareScalingLevel(1.0 / pixel_ResolutionController.getValue());
-engine.resize();
-
-
 
 // Create the scene space
 pathTracingScene = new BABYLON.Scene(engine);
@@ -384,6 +381,12 @@ engine.runRenderLoop(function ()
 
 	// first, reset cameraIsMoving flag
 	uCameraIsMoving = false;
+
+	if (beginningFlag && uSampleCounter == 1)
+	{
+		pixel_ResolutionController.setValue(0.75);
+		beginningFlag = false;
+	}
 
 	// if GUI has been used, update
 
